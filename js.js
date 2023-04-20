b7d41e2df23bc0d3e5af15360a22543fe442e03d
bennetthilberg <bennett.hilberg@gmail.com>
2023-04-19 19:43:27 -0500

    Add initial HTML and DOM declarations

---
+++ js.js
@@ -0,0 +1,8 @@
+let theForm = document.getElementById('theForm');
+let email = document.getElementById('email');
+let country = document.getElementById('country');
+let zip = document.getElementById('zip');
+let pass = document.getElementById('pass');
+let conf = document.getElementById('cpass');
+
+let inputs = [email, country, zip, pass, conf];
